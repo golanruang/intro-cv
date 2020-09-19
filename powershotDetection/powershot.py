945408a576819b9efa6f3cac2ec38dae12d7ab0c
golanruang <guang.rolan@gmail.com>
2020-09-18 18:32:05 -0700

    cropped image + changed blur for better contouring

--- powershotDetection/powershot.py
+++ powershotDetection/powershot.py
@@ -17,16 +17,11 @@ cv2.imshow("Original",image)
 # apply thresholding + gaussian blur
 image=cv2.imread(args["image"])
 image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-blurred=np.hstack([
-    cv2.GaussianBlur(image,(3,3),0),
-    cv2.GaussianBlur(image,(5,5),0),
-    cv2.GaussianBlur(image,(7,7),0)])
-# thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 15, 3)
-# cv2.imshow("Gaussian Thresh", thresh)
-
+blurred=np.hstack([cv2.GaussianBlur(image,(9,9),0)])
 edged = cv2.Canny(blurred, 30, 150)
 cv2.imshow("Edges", edged)
 (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+print("I count %d coins in this image" % (len(cnts)))
 powershots = image.copy()
 cv2.drawContours(powershots, cnts, -1, (0, 255, 0), 2)
 cv2.imshow("powershots", powershots)
